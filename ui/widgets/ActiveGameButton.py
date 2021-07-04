45fa40a742a723b8ac0ae279241841195ed2585e
BeckSM64 <becksm64@gmail.com>
2021-07-03 20:29:22 -0400

    :sparkles: add page navigator to flip through active games on join screen

--- ui/widgets/ActiveGameButton.py
+++ ui/widgets/ActiveGameButton.py
@@ -1,12 +1,22 @@
+import pygame
+from pygame.locals import *
 from ui.widgets.Button import Button
 
 class ActiveGameButton(Button):
     def __init__(self, game, width, height, x, y, color, textColor):
 
-        buttonText    = game.gameName + " - " + str(game.numPlayers)
-        self.gameName = game.gameName
-        self.gameId   = game.id
+        buttonText       = "Game Name: " + game.gameName + '\n' + "Active Players: " + str(game.numPlayers) + '\n' + "Max Players: "
+        self.gameName    = game.gameName
+        self.gameId      = game.id
 
         # Call button constructor
         Button.__init__(self, width, height, x, y, color, textColor, buttonText)
-        
+
+    def setPos(self, x, y):
+
+        # Update x and y position
+        self.x = x
+        self.y = y
+
+        # Recreate rectangle
+        self.rect = Rect(self.x, self.y, self.width, self.height)
